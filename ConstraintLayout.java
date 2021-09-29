6ef7540f353ec5e5a8b59f6a8b18a24b1e55ac6b
RickyAndriawan <82227459+RickyAndriawan@users.noreply.github.com>
2021-09-29 10:58:21 +0700

    Add files via upload

---
+++ ConstraintLayout.java
@@ -0,0 +1,14 @@
+package com.example.ricky_minggu3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
